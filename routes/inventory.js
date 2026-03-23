223e4a49b912844211086a11da4f61677f186542
N3stie <ernestmbonifacio200419@gmail.com>
2026-03-23 21:41:22 +0800

    testing for error

--- routes/inventory.js
+++ routes/inventory.js
@@ -1,3 +1,25 @@
-router.get('/error-test', (req, res) => {
-    throw new Error('Test error');
+const express = require('express');
+const router = express.Router();
+
+// Test function to make sure route works
+router.get('/detail/:id', (req, res) => {
+    res.send('Vehicle detail page works! Vehicle ID: ' + req.params.id);
 });
+
+// Custom page route - show the form
+router.get('/custom', (req, res) => {
+    res.render('custom', { title: 'Custom Order' });
+});
+
+// Custom form submission route - handle the form data
+router.post('/custom/submit', (req, res) => {
+    // For now, just show an alert and redirect
+    res.send('<script>alert("Thank you! We will contact you soon."); window.location.href="/";</script>');
+});
+
+// Test error route - triggers 500 error (ADD THIS AT THE BOTTOM)
+router.get('/error-test', (req, res) => {
+    throw new Error('This is a test error!');
+});
+
+module.exports = router;
