50a3693531f067f01357107a1eeb424decf01076
N3stie <ernestmbonifacio200419@gmail.com>
2026-03-26 21:31:12 +0800

    getting close

--- routes/inventory.js
+++ routes/inventory.js
@@ -1,4 +1,4 @@
-console.log('🔥🔥🔥 INVENTORY ROUTES FILE IS LOADING 🔥🔥🔥');
+console.log('🚗🚗 INVENTORY ROUTES FILE IS LOADING 🚗🚗');
 
 const dotenv = require('dotenv');
 dotenv.config();
@@ -9,24 +9,19 @@ router.get('/test', (req, res) => {
     res.send('TEST ROUTE IS WORKING!');
 });
 
-
-// Testing function to make sure route works
 router.get('/detail/:id', (req, res) => {
     res.send('Vehicle detail page works! Vehicle ID: ' + req.params.id);
 });
 
-// Custom page route - show the form so users can submit their custom order requests
 router.get('/custom', (req, res) => {
     res.render('custom', { title: 'Custom Order' });
 });
 
-// Custom form submission route - handle the form data and show a thank you message
 router.post('/custom/submit', (req, res) => {
     res.send('<script>alert("Thank you! We will contact you soon."); window.location.href="/";</script>');
 });
 
-// Sedan page route
-router.get('/views/sedan', (req, res) => {
+router.get('/sedan', (req, res) => {
     res.render('sedan', { title: 'Sedan Vehicles' });
 });
 
