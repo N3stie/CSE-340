21221d8c9e0c057f6f42d7ce04321ba675df6a81
N3stie <ernestmbonifacio200419@gmail.com>
2026-03-26 21:52:36 +0800

    new nav

--- routes/inventory.js
+++ routes/inventory.js
@@ -9,20 +9,34 @@ router.get('/test', (req, res) => {
     // res.send('TEST ROUTE IS WORKING!');
 });
 
+// route for vehicle detail page
 router.get('/detail/:id', (req, res) => {
     res.send('Vehicle detail page works! Vehicle ID: ' + req.params.id);
 });
 
+// route for custom order page
 router.get('/custom', (req, res) => {
     res.render('custom', { title: 'Custom Order' });
 });
 
+// route for handling custom order form submission
 router.post('/custom/submit', (req, res) => {
     res.send('<script>alert("Thank you! We will contact you soon."); window.location.href="/";</script>');
 });
 
+// route for sedan inventory page
 router.get('/sedan', (req, res) => {
     res.render('sedan', { title: 'Sedan Vehicles' });
+});
+
+// route for suv inventory page
+router.get('/suv', (req, res) => {
+    res.render('suv', { title: 'SUV Vehicles' });
+});
+
+// route for truck inventory page
+router.get('/truck', (req, res) => {
+    res.render('truck', { title: 'Truck Vehicles' });
 });
 
 module.exports = router;
